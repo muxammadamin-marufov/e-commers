f8d239c62a56e84baba517a7db37e19eb47c24c0
Muxammadamin <muxamamdamin@gmail.com>
2026-04-12 22:56:11 -0700

    Update models.py

--- market/models.py
+++ market/models.py
@@ -37,3 +37,11 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order {self.id} by {self.full_name}"
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2) # Buyurtma paytidagi narx
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
